a3fbfa8aae79bc0d837652ea6c984ee3d03b82cd
Racionalistas <m.racionalus@gmail.com>
2024-05-05 18:25:38 +0300

    videoplayer fix

--- src/pages/VideoGenerationPage/component/VideoPlayer.tsx
+++ src/pages/VideoGenerationPage/component/VideoPlayer.tsx
@@ -9,14 +9,8 @@ type VideoPlayerProps = {
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ video, message, loading }) => {
     return (
         <div className='aspect-video overflow-hidden flex justify-center items-center bg-[gray] h-[400rem] rounded-[48rem] bg-gradient-to-br from-[#D0D0D0] via-60% via-[#E0E0E0] to-[#D0D0D0] relative'>
-            <iframe className="w-full h-full"
-                src={video}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen>
-            </iframe>
-            
+            <video src={video} className='w-full h-full' controls/>
+
             {loading && <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
                 <LoaderWithMessage message={message} />
             </div>}
