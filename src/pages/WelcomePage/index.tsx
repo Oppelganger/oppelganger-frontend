c50076e7588f4dca9ec7c2bf97fff856a0fa823a
Racionalistas <m.racionalus@gmail.com>
2024-05-06 23:24:11 +0300

    upload button deleted

--- src/pages/WelcomePage/index.tsx
+++ src/pages/WelcomePage/index.tsx
@@ -1,23 +1,16 @@
 import Footer from './UI/Footer'
 import Button from '../../UI/Button'
-import { useRef } from 'react'
-import { ReactComponent as Cloud } from '../../assets/cloud.svg'
 import CompatibilityCheck from './components/CompatibilityCheck'
 import useCompatibility from './hooks/useCompatibility'
 import { useNavigate } from 'react-router-dom'
 import DividerHorizontalLine from '../../UI/DividerHorizontalLine'
 
 const WelcomePage = () => {
-  const inputRef = useRef<HTMLInputElement>(null)
   const { compatible, message, isLoading } = useCompatibility()
   const navigate = useNavigate()
 
   function handleNewProfileClick() {
     navigate('/create')
-  }
-
-  function handleUploadProfileClick() {
-    inputRef.current?.click()
   }
 
   return (
@@ -35,24 +28,13 @@ const WelcomePage = () => {
         <div className='flex flex-col gap-[32rem] items-center pb-[50rem]'>
           <DividerHorizontalLine />
 
-          <div className='flex items-center justify-center gap-[32rem] h-full px-[200rem]'>
+          <div className='flex items-center justify-center w-[855rem] gap-[32rem] h-full px-[200rem]'>
             <Button
               background='bg-[--primary]'
               text='Создать новый профиль'
               disabled={!compatible || isLoading}
               clickHandler={handleNewProfileClick}
             />
-
-            <Button
-              background='bg-[--secondary]'
-              text='Загрузить'
-              disabled={!compatible || isLoading}
-              width='w-fit'
-              clickHandler={handleUploadProfileClick}
-            >
-              <Cloud className='mr-[8rem] ml-[-4rem]' />
-              <input ref={inputRef} className='custom-input' type='file' />
-            </Button>
           </div>
 
           <Footer />
